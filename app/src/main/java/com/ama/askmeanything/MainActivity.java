0cdb77fb7273bfaac91076858b42f0f59cf13699
Ashish Tamang <aw2taman@uwaterloo.ca>
2015-11-28 23:10:38 -0500

    Activities set

--- app/src/main/java/com/ama/askmeanything/MainActivity.java
+++ app/src/main/java/com/ama/askmeanything/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ama.askmeanything;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    
+    public void onButtonTap(View v) {
+        Intent beginApp = new Intent(this, CheckQuestions.class);
+        startActivity(beginApp);
+    }
 }
